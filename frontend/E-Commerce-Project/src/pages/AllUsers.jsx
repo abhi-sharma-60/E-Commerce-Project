a8ff283ff8753dc15d9a41950aeb34954b639308
Crisonel <adityapratap1703@gmail.com>
2025-01-27 23:50:34 +0530

    Make All Users

--- frontend/E-Commerce-Project/src/pages/AllUsers.jsx
+++ frontend/E-Commerce-Project/src/pages/AllUsers.jsx
@@ -1,14 +1,75 @@
 import React, { useEffect, useState } from 'react'
+import SummaryApi from '../common'
+import {toast} from 'react-toastify'
+import moment from 'moment'
+import { FaRegEdit } from "react-icons/fa";
+import ChangeUserRole from '../components/ChangeUserRole';
 
 const AllUsers = () => {
-    const [allUser,setUsers] = useState([])
+    const [allUser,setAllUsers] = useState([])
+
+    const fetchAllUsers = async() => {
+      const fetchData = await fetch(SummaryApi.allUser.url,{
+        method : SummaryApi.allUser.method,
+        credentials : 'include'
+      })
+
+      const dataResponse = await fetchData.json()
+
+      if(dataResponse.success){
+        setAllUsers(dataResponse.data)
+      }
+
+      if(dataResponse.error){
+        toast.error(dataResponse.message)
+      }
+
+    }
 
     useEffect(() => {
-        
+        fetchAllUsers()
     },[])
 
   return (
-    <div>AllUsers</div>
+    <div className='bg-white pb-4'>
+      <table className='w-full border userTable'>
+        <thead>
+          <tr>
+            <th>Sr.</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Created At</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            allUser.map((el,index) => {
+              return(
+                <tr>
+                  <td>{index+1}</td>
+                  <td>{el?.name}</td>
+                  <td>{el?.email}</td>
+                  <td>{el?.role}</td>
+                  <td>{moment(el?.createdAt).format('LL')}</td>
+                  <td>
+                    <button className='bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green-500 hover:text-white'>
+                      <FaRegEdit/>
+                    </button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+
+      </table>
+
+
+      <ChangeUserRole/>
+    </div>
   )
 }
 
