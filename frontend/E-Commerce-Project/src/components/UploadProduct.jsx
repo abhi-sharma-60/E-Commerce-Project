59798038b1b54c6a61f00f82ef61c563ccd7198f
Crisonel <adityapratap1703@gmail.com>
2025-01-30 15:22:04 +0530

    Make Upload Product

--- frontend/E-Commerce-Project/src/components/UploadProduct.jsx
+++ frontend/E-Commerce-Project/src/components/UploadProduct.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { IoCloseSharp } from "react-icons/io5"
 import productCategory from '../helpers/productCategory'
 import { MdCloudUpload } from "react-icons/md"
+import uploadImage from '../helpers/uploadImage'
+import DisplayImage from './DisplayImage'
+import { MdDelete } from "react-icons/md";
 
 const UploadProduct = (
     onClose,
@@ -21,13 +24,27 @@ const UploadProduct = (
       const [openFullScreenImage,setOpenFullScreenImage] = useState(false)
       const [fullScreenImage,setFullScreenImage] = useState("")
 
+
       const handleOnChange = (e) => {
 
       }
 
+      const handleUploadProduct = async(e) => {
+        const file = e.target.files[0]
+
+        const uploadImageCloudinary = await uploadImage(file)
+
+        setData((prev) => {
+            return{
+                ...prev,
+                productImage : [...prev.productImage, uploadImageCloudinary.url]
+            }
+        })
+      }
+
 
   return (
-    <div className='fixed w-full h-full bg-slate-200 bg-opacity-35 top-0 left-0 right-0 bottom-0 flex justify-center items-center'>
+    <div className='fixed w-full h-full bg-slate-200 bg-opacity-35 top-0 left-0 right-0 bottom-0 flex justify-center items-center cursor-pointer'>
         <div className='bg-white p-4 rounded w-full max-w-2xl h-full max-h-[80%] overflow-hidden'>
             
             <div className='flex justify-between items-center pb-3'>
@@ -69,21 +86,60 @@ const UploadProduct = (
                 </select>
 
                 <label htmlFor="productImage" className='mt-3'>Product Image : </label>
-                <div className='p-2 bg-slate-100 border rounded h-32 w-full flex justify-center items-center'>
-                    <div className='text-slate-500 flex justify-center items-center flex-col gap-2'>
-                        <span className='text-4xl'><MdCloudUpload/></span>
-                        <p className='text-sm'>Upload Product Image</p>
-                        <input type='file' id='uploadImageInput'  className='hidden' onChange={handleUploadProduct}/>
-                    </div>
-                </div>
-
+                <label htmlFor='uploadImageInput'>
+                    <div className='p-2 bg-slate-100 border rounded h-32 w-full flex justify-center items-center'>
+                        
+                            <div className='text-slate-500 flex justify-center items-center flex-col gap-2'>
+                                <span className='text-4xl'><MdCloudUpload/></span>
+                                <p className='text-sm'>Upload Product Image</p>
+                                <input type='file' id='uploadImageInput'  className='hidden' onChange={handleUploadProduct}/>
+                            </div>
+                        </div>
+                </label>
                 <div>
-                    <img src="" width={80} height={80} className='bg-slate-100 border'/>
+                    {
+                        data?.productImage[0] ? (
+                            <div className='flex items-center gap-2'>
+                                {
+                                    data?.productImage.map(el => {
+                                        return(
+                                            <img 
+                                            src={el} 
+                                            alt={el} 
+                                            width={80} 
+                                            height={80} 
+                                            className='bg-slate-100 border cursor-pointer' 
+                                            onClick={() => {
+                                                setFullScreenImage(el)
+                                                setOpenFullScreenImage(true)
+                                            }}/>
+                                        )
+                                    })
+                                }
+                            </div>
+                        ) : (
+                            <p className='text-red-600 text-xs'>*Please upload product image</p>
+                        )
+                    }
+                    
                 </div>
 
+                <button className='px-3 py-2 bg-red-600 text-white mb-10 hover:bg-red-700'>Upload Product</button>
 
             </form>
+
+            
+
+
         </div>
+
+        {/** display image full screen */}
+        {
+        openFullScreenImage && (
+          <DisplayImage onClose={()=>setOpenFullScreenImage(false)} imgUrl={fullScreenImage}/>
+        )
+       }
+
     </div>
   )
 }
