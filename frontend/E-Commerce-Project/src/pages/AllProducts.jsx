6398d4a2e390adfe07b3f961f6666f124a62fde2
Crisonel <adityapratap1703@gmail.com>
2025-02-01 16:49:45 +0530

    Complete Admin Page

--- frontend/E-Commerce-Project/src/pages/AllProducts.jsx
+++ frontend/E-Commerce-Project/src/pages/AllProducts.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UploadProduct from '../components/UploadProduct'
-import { useState } from 'react'
 import SummaryApi from '../common'
 import AdminProductCard from '../components/AdminProductCard'
 
@@ -12,7 +11,7 @@ const AllProducts = () => {
     const response = await fetch(SummaryApi.allProduct.url)
     const dataResponse = await response.json()
 
-    //console.log("product data",dataResponse)
+    console.log("product data",dataResponse)
 
     setAllProduct(dataResponse?.data || [])
   }
@@ -20,21 +19,16 @@ const AllProducts = () => {
   useEffect(()=>{
     fetchAllProduct()
   },[])
-
-
-
-
-
-
+  
   return (
     <div>
-      <div className='bg-white py-2 px-4 flex justify-between items-center'>
+        <div className='bg-white py-2 px-4 flex justify-between items-center'>
             <h2 className='font-bold text-lg'>All Product</h2>
             <button  className='border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full ' onClick={()=>setOpenUploadProduct(true)}>Upload Product</button>
-      </div>
+        </div>
 
-      {/**all product */}
-      <div className='flex items-center flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll'>
+        {/**all product */}
+        <div className='flex items-center flex-wrap gap-5 py-4 h-[calc(100vh-190px)] overflow-y-scroll'>
           {
             allProduct.map((product,index)=>{
               return(
@@ -45,13 +39,17 @@ const AllProducts = () => {
           }
         </div>
 
-     {/**upload product */}
-      {
-        openUploadProduct && (
-          <UploadProduct onClose={()=>setOpenUploadProduct(false)}/>
-        )
-      }
 
+
+
+
+        {/**upload product component */}
+        {
+          openUploadProduct && (
+            <UploadProduct onClose={()=>setOpenUploadProduct(false)} fetchData={fetchAllProduct}/>
+          )
+        }
+      
 
     </div>
   )
