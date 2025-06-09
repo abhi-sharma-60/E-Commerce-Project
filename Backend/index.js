583201585f2dfe15cdd177c7fbf9d2f45230501b
Abhi Sharma <abhi.sharma.abhi.60@gmail.com>
2025-06-09 11:45:55 +0530

    changes in backend/index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const router = require("./api/index");
-const cookieParser = require("cookie-parser");
+
+dotenv.config();
+
+if (!process.env.FRONTEND_URL || !process.env.MONGO_URI) {
+  throw new Error("Missing required environment variables");
+}
 
 const app = express();
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -13,9 +20,10 @@ app.use(
   })
 );
 app.use(express.json());
-app.use("/api", router);
 app.use(cookieParser());
-const PORT = 8080 || process.env.PORT;
+app.use("/api", router);
+
+const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
